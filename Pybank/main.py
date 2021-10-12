6aef2c15dbc0a1b891ce7b4195df54d54c7e54d1
Mona Sidhu <mona_leo2@yahoo.com>
2021-10-13 00:38:18 +0800

    initial commit

---
+++ Pybank/main.py
@@ -0,0 +1,74 @@
+import os
+import csv
+filepath = "/Users/gurbindesidhu/Desktop/Pythonhomework/pythonchallenges/Pybank.py/Resources/budget_data.csv"
+#filepath = "./Resources/budget_data.csv"
+profit = []
+monthly_changes = []
+date = []
+count = 0
+total_profit = 0
+total_change_profits = 0
+initial_profit = 0
+average_change_profits = 0
+total_loss=0
+initial_loss = 0
+total_change_losses=0
+average_change_losses =0
+total_months = 0
+total_net = 0
+average_change=0
+
+
+with open(filepath) as csv_file:
+
+    #print (csv_file)
+
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    csv_header = next(csv_reader)
+    #print(csv_header)
+    
+    for row in csv_reader:
+          print(row)
+          total_months = total_months + 1
+          total_net = total_net + int(row[1])
+          #average_change= total_net/total_months
+          count=count+1
+          date.append(row[0])
+          total_profit = total_profit + int(row[1])
+          final_profit=int(row[1])
+          monthly_change_profit=final_profit - initial_profit
+          monthly_changes.append(monthly_change_profit)
+          total_change_profits=total_change_profits + monthly_change_profit
+          initial_profit = final_profit
+average_change_profits = (total_change_profits/(count))
+          
+        #   total_loss = total_loss + int(row[1])
+        #   final_loss=int(row[1])
+        #   monthly_change_loss=final_loss - initial_loss
+        #   monthly_changes.append(monthly_change_loss)
+        #   total_change_losses=total_change_losses + monthly_change_loss
+        #   initial_loss = final_loss
+        #   average_change_losses = (total_change_losses/count)
+          
+          #average_change=average_change_profits-average_change_losses
+greatest_increase_profits = max(monthly_changes)
+greatest_decrease_profits = min(monthly_changes)
+        #   increase_date = date[monthly_changes.index(greatest_increase_profits)]
+        #   decrease_date = date[monthly_changes.index(greatest_decrease_profits)]
+
+
+print(monthly_change_profit)
+print(f"total months:{total_months}")
+print(f"total net:{total_net}")
+print(f"average change profits:{average_change_profits}")
+#print(f"average change losses:{average_change_losses}")
+print(f"greatest increase profits:{greatest_increase_profits}")
+print(f"greatest decrease profits:{greatest_decrease_profits}")
+# print(f"increase date:{increase_date}")
+# print(f"decrease date:{decrease_date}")
+
+
+    # track total months
+    # track net change
+    # Greatest increase
+    # greatest decrease  
---
+++ Pypoll/main.py
@@ -0,0 +1,86 @@
+import os
+import csv
+
+# filepath = "/Users/gurbindesidhu/Desktop/Pythonhomework/pythonchallenges/Pypoll.py/Resources/election_data.csv"
+filepath = "./Resources/election_data.csv"
+filepathout = "./analysis/Election_Results.txt"
+count = 0
+candidate_list = []
+popular_candidate = []
+candidate_choice = []
+vote_count = {}
+vote_percent = []
+total_votes = 0
+candidate_name=0
+winning_count=0
+#voter_output=[]
+
+#def vote_percent(candidate_name):
+
+with open(filepath,newline='') as csv_file:
+    csv_reader = csv.reader(csv_file,delimiter=",")
+    csv_header = next(csv_reader)
+    
+    for row in csv_reader:
+        count = count + 1
+        candidate_name=row[2]
+        #total_number += 1
+        #print('candidate name', candidate_name)
+    
+        #candidate_list.append(candidate_name)
+        
+
+        if candidate_name not in candidate_list:
+            candidate_list.append(candidate_name)
+            vote_count[candidate_name] = 1
+        else:
+            vote_count[candidate_name] = vote_count[candidate_name] + 1
+    # print(vote_count)
+    for candidate in vote_count:
+        votes=vote_count.get(candidate)
+        vote_percent= (float(votes)/float(count))*100
+        if(votes>winning_count):
+            winning_count=votes
+            winning_candidate= candidate
+        voter_output= f"{candidate}:{vote_percent:3f}% ({votes})\n"
+
+        print(voter_output)             
+   
+    Election_Results = (
+
+       f"----------------------------------\n"
+        f"Election_Results\n"
+
+        f"Total votes:{count}\n"
+        f"----------------------------------"
+    )
+
+    print(Election_Results,end="")
+
+    winning_candidate_summary =(
+    f"-------------------------\n"
+    f"Winner:{winning_candidate}\n" 
+    f"---------------------------\n")
+    print(winning_candidate_summary)
+
+ #Creating output file:
+with open(filepathout,"w") as txt_file:
+
+    
+    txt_file.write(Election_Results)
+
+
+    # txt_file.write(vote_count)
+    
+   
+    txt_file.write(winning_candidate_summary)
+    
+
+
+
+
+
+
+
+    
+   
